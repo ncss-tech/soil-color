295073d40ddd345ab38b148b37e262207fa41eb0
Dylan Beaudette <debeaudette@ucdavis.edu>
2019-05-23 16:16:04 -0700

    updates

--- Nix/SOP/Tile_Calibration_Test.R
+++ Nix/SOP/Tile_Calibration_Test.R
@@ -1,7 +1,7 @@
 # load required libraries
 library(colorspace)
 library(aqp)
-library(plyr)
+library(farver)
 
 # load CSV files  
 x <- read.csv('Tile_Calibration_Test.csv', stringsAsFactors = FALSE)
@@ -19,6 +19,19 @@ plot(x.lab, cex=5)
 # convert to sRGB
 x.srgb <- as(x.lab, 'sRGB')
 plot(x.srgb, cex=5)
+
+# graphical comparison
+x.hex <- hex(x.srgb)
+par(bg='black')
+swatchplot(x.hex)
+
+# CIE delta-E00
+d <- compare_colour(coords(x.lab)[, 1:3], from_space='LAB', method='CIE2000', white_from='D65')
+d[d == 0] <- NA
+par(bg='white')
+hist(d, breaks = 10)
+
+# ... these are all very small differences, below the level of perception
 
 # convert to closest Munsell chip
 # the @coords extracts just the color coordinates as a matrix
